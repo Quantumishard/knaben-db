95cfc344e2efe18866fd3d12eb084aff2c852332
mikmcdanbyeee55 <80829378+mikmcdanbyeee55@users.noreply.github.com>
2023-09-05 02:04:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const toStream = async (parsed, uri, tor, type, s, e) => {
   if (!parsed.files && uri.startsWith("magnet")) {
     try {
       const engine = torrentStream("magnet:" + uri, {
-        connections: 7, // Limit the number of connections/streams
+        connections: 5, // Limit the number of connections/streams
       });
 
       const res = await new Promise((resolve, reject) => {
