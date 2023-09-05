9f2d22ba302af9d420f6bd1a1498ec41c8005040
mikmcdanbyeee55 <80829378+mikmcdanbyeee55@users.noreply.github.com>
2023-09-05 02:17:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -174,7 +174,7 @@ const host = "http://100.12.26.164:9117";
 const apiKey = "b3f8f3fb4rtt4vcsml7cz82dtkjbj3df";
 
 let fetchTorrent = async (query) => {
-  let url = `${host}/api/v2.0/indexers/all/results?apikey=${apiKey}&Query=${query}&Category[]=2000&Category[]=5000&Tracker[]=bitsearch`;
+  let url = `${host}/api/v2.0/indexers/all/results?apikey=${apiKey}&Query=${query}&Category[]=2000&Category[]=5000&Tracker[]=limetorrents&Tracker[]=bitsearch`;
 
   try {
     const response = await fetch(url, {
