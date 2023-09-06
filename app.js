42b3646c71f9b2cd218d881673cdcff44d43c1e1
dantheman <80829378+mikmcdanbyeee55@users.noreply.github.com>
2023-09-06 05:33:50 +0100

    Update app.js

--- app.js
+++ app.js
@@ -212,8 +212,8 @@ let stream_results = [];
 let torrent_results = [];
 
 const host1 = {
-  hostUrl: "http://81.0.219.102:9117",
-  apiKey: "cmj23dexby9u04d3531mm03iikfxgbkr",
+  hostUrl: "http://94.61.74.253:9117",
+  apiKey: "e71yh2n0fopfnyk2j2ywzjfa3sz4xv8d",
 };
 
 const host2 = {
@@ -223,7 +223,7 @@ const host2 = {
 
 const fetchTorrentFromHost1 = async (query) => {
   const { hostUrl, apiKey } = host1;
-  const url = `${hostUrl}/api/v2.0/indexers/all/results?apikey=${apiKey}&Query=${query}&Category%5B%5D=2000&Category%5B%5D=2010&Category%5B%5D=2020&Category%5B%5D=2040&Category%5B%5D=2045&Category%5B%5D=2070&Category%5B%5D=5000&Category%5B%5D=5020&Category%5B%5D=5030&Category%5B%5D=5040&Category%5B%5D=5045&Category%5B%5D=5050&Category%5B%5D=5060&Category%5B%5D=5080&Category%5B%5D=8000&Tracker%5B%5D=rutracker`;
+  const url = `${hostUrl}/api/v2.0/indexers/all/results?apikey=${apiKey}&Query=${query}&Category%5B%5D=2000&Category%5B%5D=5000&Category%5B%5D=8000&Tracker%5B%5D=torrentscsv`;
 
   try {
     const response = await fetch(url, {
